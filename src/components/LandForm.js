4677131a38d38df9c7b7bd2f2f0339774516598b
mrnayak25 <mrnayak27@gmail.com>
2024-06-23 07:35:10 +0530

    hjkjhkjhkjhkjhk

--- src/components/LandForm.js
+++ src/components/LandForm.js
@@ -538,19 +538,6 @@ const LandForm = () => {
                     placeholder="Property Zip Code"
                     required
                   />
-                  <label htmlFor="propertyType" className="block mb-1 text-sm font-medium text-gray-900 ">
-                    Property Type
-                  </label>
-                  <input
-                    type="text"
-                    name="propertyType"
-                    id="propertyType"
-                    className="bg-gray-50 border mb-3 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    value={land.propertyType}
-                    onChange={handleChange}
-                    placeholder="Property Type"
-                    required
-                  />
                   <label htmlFor="totalArea" className="block mb-1 text-sm font-medium text-gray-900 ">
                     Total Area
                   </label>
