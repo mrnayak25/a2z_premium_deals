543c8aca2dc38533c30dea3318235648d1bd8cb3
mrnayak25 <mrnayak27@gmail.com>
2024-07-03 16:26:15 +0530

    modified upload

--- src/components/LandForm.js
+++ src/components/LandForm.js
@@ -155,7 +155,7 @@ const LandForm = () => {
   {loading ? (
     <Loading />
   ) : (
-    <form onSubmit={handleSubmit} className="mt-10 max-w-2xl mx-auto p-6 bg-white shadow rounded-lg ">
+    <form onSubmit={handleSubmit} className=" my-10 max-w-5xl mx-auto p-6 bg-gray-200 shadow rounded-lg ">
       <div className="rounded-lg p-6">
         <h1 className="text-black text-center text-2xl font-semibold mb-4 underline">Post Your Property</h1>
         <p className="text-black text-center mb-6">
@@ -170,7 +170,7 @@ const LandForm = () => {
             <label className="block mb-2 text-sm font-medium text-black">
               Is this your property, or are you an agent or builder?
             </label><br/>
-            <div className="flex justify-between">
+            <div className="flex justify-between mx-52">
               {["Individual", "Agent", "Builder"].map((type) => (
                 <div className="flex items-center" key={type}>
                   <input
@@ -208,9 +208,8 @@ const LandForm = () => {
                 />
               </div>
             ))}
-          </div>
 
-          <div className="mb-4">
+          <div className="mb-4 md:col-span-2">
             <label htmlFor="state" className="block mb-1 text-sm font-medium text-black">
               State
             </label>
@@ -225,14 +224,15 @@ const LandForm = () => {
               {/* Add other state options here */}
             </select>
           </div>
+          </div>
         </div>
 
         {/* Property Details Section */}
-        <div className="mb-6">
+        <div className="">
           <h2 className="text-black text-xl font-semibold mb-4">Property Details</h2>
           <div className="mb-4">
             <label className="block mb-2 text-sm font-medium text-black">Is this a sell, rent, or lease?</label>
-            <div className="flex justify-between">
+            <div className="flex justify-between md:mx-52">
               {["SELL", "RENT", "LEASE"].map((option) => (
                 <div className="flex items-center" key={option}>
                   <input
@@ -252,6 +252,7 @@ const LandForm = () => {
             </div>
           </div>
 
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
           {[
             {
               id: "location",
@@ -384,21 +385,6 @@ const LandForm = () => {
             </select>
           </div>
 
-          <div className="mb-4">
-            <label htmlFor="description" className="block mb-1 text-sm font-medium text-black">
-              Description
-            </label>
-            <textarea
-              id="description"
-              name="description"
-              value={land.description}
-              onChange={handleChange}
-              placeholder="Enter a brief description of the property"
-              rows="4"
-              className="w-full p-2 border rounded bg-white text-black"
-              required
-            />
-          </div>
 
           <div className="mb-4">
             <label htmlFor="propertyImages" className="block mb-1 text-sm font-medium text-black">
@@ -414,6 +400,24 @@ const LandForm = () => {
             />
           </div>
 
+          <div className="mb-4 md:col-span-3">
+            <label htmlFor="description" className="block mb-1 text-sm font-medium text-black">
+              Description
+            </label>
+            <textarea
+              id="description"
+              name="description"
+              value={land.description}
+              onChange={handleChange}
+              placeholder="Enter a brief description of the property"
+              rows="4"
+              className="w-full p-2 border rounded bg-white text-black"
+              required
+            />
+          </div>
+
+          </div>
+
           <div className="flex items-center mb-4">
             <input
               id="termsConditions"
@@ -426,11 +430,11 @@ const LandForm = () => {
             <label htmlFor="termsConditions" className="ml-2 text-sm font-medium text-black">
               I agree to the terms and conditions
             </label>
-          </div>
+         </div>
 
           <button
             type="submit"
-            className="w-full py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700 transition duration-200">
+            className="w-52 py-2  bg-blue-600 text-white font-semibold rounded hover:bg-blue-700 transition duration-200">
             Submit
           </button>
         </div>
