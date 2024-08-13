ade157ad7ea50da7c735ea21a99d42916c476588
SVVAAP <workwithsvvaao@gmail.com>
2024-08-13 21:22:54 +0530

    hiiiiiii

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,9 +6,9 @@ import { FaHandshake } from "react-icons/fa";
 
 const Services = () => {
   return (
-    <div className="w-11/12 m-auto flex flex-col md:flex-row justify-between items-center md:space-x-10 space-y-10 md:space-y-0 py-10">
+    <div className="m-auto flex flex-col md:flex-row justify-around items-center md:space-x-10 space-y-10 md:space-y-0 py-10">
     {/* right side */}
-    <div className=" flex space-x-4 md:space-x-10">
+    <div className=" flex space-x-4 md:space-x-10 p-5">
       <div className="mt-10 space-y-4">
         <div className=" w-48 flex flex-col items-center bg-white text-center p-5 drop-shadow-2xl rounded-md">
           <MdHomeRepairService size={"1.8rem"} />
@@ -45,9 +45,9 @@ const Services = () => {
     </div>
 
     {/* left side */}
-    <div className=" w-1/2 md:1/2 space-y-5">
-      <h1 className="text-6xl font-bold">
-        Know <span className=" text-orange-600">ur Services</span>
+    <div className=" w-2/3 md:1/2 space-y-5">
+      <h1 className="text-6xl font-bold items-center">
+        Know <span className=" text-orange-600">Our Services</span>
       </h1>
       <p className="  text-gray-500">
         We are a leading Real Estate company, specializing in property sales,
@@ -58,7 +58,7 @@ const Services = () => {
         for all your real estate requirements
       </p>
       <button className=" px-5 py-2 bg-orange-600 text-white rounded-md hover:bg-orange-700">
-        Read More
+        Get Started !
       </button>
     </div>
   </div>
