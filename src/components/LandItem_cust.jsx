2edc429125d8c712a78565ae8addc3d1f8275ab8
SVVAAP <workwithsvvaao@gmail.com>
2024-09-17 23:10:46 +0530

    hii

--- src/components/LandItem_cust.jsx
+++ src/components/LandItem_cust.jsx
@@ -14,13 +14,13 @@ const LandItem = ({ land, setId, index }) => {
   }
 
   return (
-    <div className="object-cover transition-transform duration-300 transform hover:scale-105 ">
-      <div className="flex flex-col items-start gap-2.5 p-3 rounded-lg mx-auto transition-transform duration-300 ease-in hover:scale-105 hover:bg-gradient-to-b hover:from-transparent hover:to-blue-100/75 hover:shadow-xl">
-        <div className="max-h-xl text-sm">
-          <div className="absolute top-0.2 left-4 z-10" data-aos="fade-down" data-aos-delay="400">
+    <div className="w-64 h-80 object-cover transition-transform duration-300 transform hover:scale-105">
+      <div className="flex flex-col items-start gap-2.5 p-3 rounded-lg mx-auto w-full h-full transition-transform duration-300 ease-in hover:scale-105 hover:bg-gradient-to-b hover:from-transparent hover:to-blue-100/75 hover:shadow-xl">
+        <div className="relative w-full h-full">
+          <div className="absolute top-2 left-4 z-10" data-aos="fade-down" data-aos-delay="400">
             <button className="text-white bg-orange-500 p-2 font-medium rounded-br-lg rounded-tl-lg">{index}</button>
           </div>
-          <div className="absolute top-0.1 right-4 z-10" data-aos="fade-down" data-aos-delay="400">
+          <div className="absolute top-2 right-4 z-10" data-aos="fade-down" data-aos-delay="400">
             <RWebShare
               data={{
                 text: `Check out this property on A2Z PREMIUM DEALS \n Title: ${land.title}\n Description: ${land.description}\n Price: Rs.${formatPrice(land.price)}`,
@@ -35,7 +35,7 @@ const LandItem = ({ land, setId, index }) => {
             </RWebShare>
           </div>
           <Link to={`/viewproperty/${land.id}`}>
-            <img src={land.imageUrls[0]} alt="home" className="rounded w-full h-1/2" data-aos="fade-down" data-aos-delay="200" />
+            <img src={land.imageUrls[0]} alt="home" className="rounded w-full h-40 object-cover" data-aos="fade-down" data-aos-delay="200" />
           </Link>
         </div>
 
